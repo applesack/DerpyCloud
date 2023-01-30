cd0aacfd8d8da1ce20ef83ccaa0915d131c223f1
applesack <2383955734@qq.com>
2023-01-30 22:05:14 +0800

    (后端/构建文件) 添加基本依赖项

---
+++ derpy-server/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "xyz.scootaloo"
+version = "0.1"
+
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("io.vertx:vertx-core:4.3.6")
+    implementation("io.vertx:vertx-web:4.3.6")
+    implementation("io.vertx:vertx-lang-kotlin:4.3.6")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.3.6")
+
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-core:1.2.11")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        allWarningsAsErrors = true
+    }
+}
+
+tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf(
+            "-Xjvm-default=enable"
+        )
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
---
+++ derpy-server/src/main/kotlin/xyz/scootaloo/server/Main.kt
@@ -0,0 +1,14 @@
+package xyz.scootaloo.server
+
+/**
+ * @author AppleSack
+ * @since 2023/01/30
+ */
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("hello world")
+    }
+
+}
