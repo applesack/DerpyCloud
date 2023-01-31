c60bf69896bbbb4658cfe5836f483678900eefe8
applesack <flutterdash@qq.com>
2023-01-31 13:52:43 +0800

    (后端/测试) 基本代码

--- derpy-server/src/test/kotlin/xyz/scootaloo/server/TestMain.kt
+++ derpy-server/src/test/kotlin/xyz/scootaloo/server/TestMain.kt
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test
 
 /**
  * @author AppleSack
- * @since 2023/01/31
+ * @since  2023/01/31
  */
 class TestMain {
 
     @Test
-    fun main() {
-        println("hello test")
+    fun test() {
+        println("hello")
     }
 
 }
